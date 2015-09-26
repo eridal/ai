a34d9fc6aeb5488489381cafa0365cd66ba37d12
Martin Scotta <martinscotta@gmail.com>
2015-09-25 22:44:43 -0300

    matrix operations

--- src/eridal/ai/utils/ArrayMath.java
+++ src/eridal/ai/utils/ArrayMath.java
@@ -10,6 +10,32 @@ public class ArrayMath {
         return sum;
     }
 
+    public static double[][] product(final double[] v1, double[] v2) {
+
+        double[][] m = new double[v1.length][v2.length];
+
+        for (int i = 0; i < v1.length; i++) {
+            for (int j = 0; j < v2.length; j++) {
+                m[i][j] = v1[i] * v2[j];
+            }
+        }
+
+        return m;
+    }
+
+    public static double[][] sum(final double[][] m1, double[][] m2) {
+
+        double[][] m = new double[m1.length][m1[0].length];
+
+        for (int i = 0; i < m1.length; i++) {
+            for (int j = 0; j < m1[i].length; j++) {
+                m[i][j] = m1[i][j] + m2[i][j];
+            }
+        }
+
+        return m;
+    }
+
     public static int sum(final int[] values) {
         int sum = 0;
         for (int v : values) {
