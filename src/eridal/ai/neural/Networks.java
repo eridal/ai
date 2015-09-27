dcdc050c0727e12d82616e21b8e200da3ed1c020
Martin Scotta <martinscotta@gmail.com>
2015-09-27 02:40:06 -0300

    cleanup

--- src/eridal/ai/neural/Networks.java
+++ src/eridal/ai/neural/Networks.java
@@ -6,10 +6,23 @@ import eridal.ai.utils.Filter;
 public class Networks {
 
     /**
-     * Returns a Perceptron network.
+     * @param bias activation threshold
+     * @param layers quantities of neurons to create
+     */
+    public static Network create(double bias, int...layers) {
+        return new NetworkBuilder()
+            .layers(layers)
+            .bias(bias)
+            .squash(Squashs.SIGMOID)
+            .filter(Filter.HALF_STEP)
+            .build();
+    }
+
+    /**
+     * Creates a Perceptron network.
      *
-     * @param bias activation bias
-     * @return the created Perceptron
+     * @param bias activation threshold
+     * @return the created Perceptron network
      */
     public static Network percepton(double bias) {
         double w0 = Synapse.randomWeight();
@@ -17,7 +30,15 @@ public class Networks {
         return percepton(bias, w0, w1);
     }
 
-
+    /**
+     * Creates a Perceptron network.
+     *
+     * @param bias activation threshold
+     * @param w0 weight for first input
+     * @param w1 weight for second input
+     *
+     * @return the created Perceptron network
+     */
     public static Network percepton(double bias, double w0, double w1) {
 
         final Neuron n0 = new Neuron(0);
@@ -35,23 +56,32 @@ public class Networks {
     }
 
     /**
-     * @param bias activation bias
-     * @param layers 
-     * @return
+     * Creates a Hopfield network
+     *
+     * @param patterns The patterns this network should recognize;
+     *
+     * @return The created network
+     *
+     * @throws IllegalArgumentException when all patterns not have the same length.
      */
-    public static Network backPropagation(double bias, int...layers) {
-        return new NetworkBuilder()
-            .layers(layers)
-            .bias(bias)
-            .squash(Squashs.SIGMOID)
-            .filter(Filter.HALF_STEP)
-            .build();
-    }
+    public static Network hopfield(double[] ...patterns) {
+ 
+        if (patterns.length == 0 || patterns[0].length == 0) {
+            throw new IllegalArgumentException("missing patterns");
+        }
 
-    public static Network hopfield(int size, double[] v0, double[] v1) {
+        int size = patterns[0].length;
 
-        if (size < 1 || v0.length != size || v1.length != size) {
-            throw new IllegalArgumentException();
+        double[][] weights = new double[size][size];
+
+        for (double[] p : patterns) {
+
+            if (p.length != size) {
+                throw new IllegalArgumentException("wrong pattern size");
+            }
+
+            double[][] m = ArrayMath.product(p, p);
+            weights = ArrayMath.sum(weights, m);
         }
 
         final Neuron[] i = new Neuron[size];
@@ -62,17 +92,13 @@ public class Networks {
             o[k] = new Neuron(k + size);
         }
 
-        final double[][] m0 = ArrayMath.product(v0, v0);
-        final double[][] m1 = ArrayMath.product(v1, v1);
-        final double[][] m  = ArrayMath.sum(m0, m1);
-
         for (int s = o.length; s-- > 0;) {
             for (int t = o.length; t-- > 0;) {
                 if (s == t) {
                     i[s].synapseTo(o[t], 1.0);
                 }
                 else {
-                    o[s].synapseTo(o[t], m[s][t]);
+                    o[s].synapseTo(o[t], weights[s][t]);
                 }
             }
         }
