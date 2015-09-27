709ac16bd4dea31e415c8ee7631bd3212badc799
Martin Scotta <martinscotta@gmail.com>
2015-09-27 05:27:24 -0300

    better trainner

--- src/eridal/ai/neural/Trainer.java
+++ src/eridal/ai/neural/Trainer.java
@@ -1,83 +1,133 @@
 package eridal.ai.neural;
 
-public abstract class Trainer {
+import java.util.ArrayList;
+import java.util.List;
+
+import eridal.ai.utils.ArrayMath;
+
+public class Trainer {
+
+    final Network network;
+
+    final double η;
+
+    public List<double[]> trainX = new ArrayList<>();
+    public List<double[]> trainY = new ArrayList<>();
+
+    public List<double[]> testX = new ArrayList<>();
+    public List<double[]> testY = new ArrayList<>();
+
+    public Trainer(Network network, double η) {
+        this.network = network;
+        this.η = η;
+    }
+
+    public Trainer train(double[] x, double[] y) {
+        this.trainX.add(x);
+        this.trainY.add(y);
+        return this;
+    }
+
+    public Trainer test(double[] x, double[] y) {
+        this.testX.add(x);
+        this.testY.add(y);
+        return this;
+    }
+
+    static final double ERROR_MIN = 1e-7;
 
     /**
-     * Train the network until error drop.
+     * Train the network until error drop to minimum.
      * 
-     * @param network the network to train
-     * @param η learn rate speed
-     * @param inputs input values
-     * @param outputs expected output values
-     * @param e  minimum error threshold value
+     * @param error minimum error threshold value
      * 
-     * @return iterations required to train the network
+     * @return error rate
      */
-    public static long train(Network network,
-                             double η,
-                             double[][] inputs,
-                             double[][] outputs,
-                             double error) {
-
-         long k = 0;
-         double sum;
-
-         do {
-             sum = 0;
-             for (int i = inputs.length; i-- >0; k += 1) {
-                 double e = teach(network, η, inputs[i], outputs[i]);
-                 sum += Math.abs(e);
-             }
-         } while (error < sum);
-
-         return k;
-     }
-
-    public static long train(Network network,
-                             double η,
-                             double[][] inputs,
-                             double[][] outputs) {
-        return train(network, η, inputs, outputs, 0.0);
-    }
-
-    public static long train(Network network,
-                             double[][] inputs,
-                             double[][] outputs,
-                             double e) {
-        return train(network, 0.01, inputs, outputs, e);
-    }
-
-    public static long train(Network network,
-                             double[][] inputs,
-                             double[][] outputs) {
-        return train(network, 0.01, inputs, outputs, 0.0);
+    public double go() {
+        return go(-1);
     }
 
     /**
-     * Teach a given set of inputs to some network 
+     * Train the network until error drop,
+     * or it wont get any better.
+     * 
+     * @param error minimum error threshold value
+     * 
+     * @return error rate
+     */
+    public double go(double error) {
+
+        double sum = 0;
+        double avg = error + 1;
+
+        double[] epoch = new double[(trainX.size() + testX.size()) * 15];
+
+        for (int e = 0; error < avg; ) {
+
+            for (int k = trainX.size(); k-- > 0; e++) {
+                double err = teach(trainX.get(k), trainY.get(k));
+                sum -= epoch[e + k];
+                sum += epoch[e + k] = err;
+            }
+
+            for (int k = testX.size(); k-- > 0; e++) {
+                double err = errorDiff(testX.get(k), testY.get(k));
+                sum -= epoch[e + k];
+                sum += epoch[e + k] = err;
+            }
+
+            avg = sum / trainX.size();
+
+            if (e == epoch.length) {
+                double err = ArrayMath.sum(epoch) / epoch.length;
+                if (err < ERROR_MIN) {
+                    return err;
+                }
+                e = 0;
+            }
+
+        }
+
+        return avg;
+    }
+
+    /**
+     * Teach a given set of inputs to some network
      *
-     * @param net The network to be trained
-     *
-     * @param η learn speed rate
      * @param x input values
      * @param y expected output values
      *
-     * @return error rate delta
+     * @return error delta
      */
-    public static double teach(Network net, double η, double[] x, double[] y) {
+    private double teach(double[] x, double[] y) {
+        double[] err = errorVector(x, y);
+        return network.propagate(η, err);
+    }
 
-        double[] r = net.execute(x);
+    private double[] errorVector(double[] x, double[] y) {
+
+        double[] r = network.execute(x);
 
         if (r.length != y.length) {
             throw new IllegalArgumentException();
         }
 
-        double[] Δerr = new double[y.length];
+        double[] err = new double[y.length];
 
         for (int k = y.length; k-- > 0;) {
-            Δerr[k] = r[k] - y[k];
+            err[k] = r[k] - y[k];
         }
 
-        return net.propagate(η, Δerr);
+        return err;
     }
+
+    private double errorDiff(double[] x, double[] y) {
+        double[] err = errorVector(x, y);
+        double r = 0;
+        for (double e : err) {
+            r += e;
+        }
+        return r;
+    }
+
 }
